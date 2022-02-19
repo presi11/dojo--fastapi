ccfe7cc282ed6170d925f2b5287f5b92d04bcada
Presi <presi@pop-os.localdomain>
2022-02-19 11:04:10 -0500

    upload files

---
+++ main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+from starlette.responses import RedirectResponse
+from routes import book_routes
+
+
+
+app = FastAPI()
+
+app.include_router(book_routes.route)
+
+@app.get('/')
+def home():
+    res = RedirectResponse(url='/docs')
+    return res
---
+++ repository/book_repository.py
@@ -0,0 +1,35 @@
+from models.book_entity import BookEntity
+from db.data import book_data
+
+books = book_data.get("books")
+
+
+def get_all_books():
+    return books
+
+
+def get_book_by_isbn(isbn: str):
+    for book in books:
+
+        if book.get("isbn") == isbn:
+            return book
+    return None
+
+
+def create_book(book: BookEntity):
+    books.append(book.dict())
+    return book
+
+
+def update_book(isbn: str, book: BookEntity):
+    index = books.index(get_book_by_isbn(isbn))
+    books[index].update(book.dict(exclude_unset=True))
+    return books[index]
+
+
+def delete_book(isbn: str):
+    book = get_book_by_isbn(isbn)
+    if not book:
+        return None
+    books.remove(book)
+    return book
